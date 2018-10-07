756f49407e824937e9d571947ee59b6e5b733634
Pär Nygren <par.a.nygren@gmail.com>
2018-10-07 18:00:53 +0200

    Adding Pagination

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
 
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 import { Note } from '../../models/note';
 
@@ -9,7 +9,7 @@ import { Note } from '../../models/note';
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent implements OnInit {
+export class NotesComponent implements OnInit, AfterViewInit {
 
   @Input() notes: Note[];
 
@@ -18,8 +18,14 @@ export class NotesComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   ngOnInit() {
     this.dataSource = new MatTableDataSource<Note>(this.notes);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
